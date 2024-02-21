2beb3c8c056c1cb9303dbc382e33a85b856f3e94
golden lira <empirecheat17@gmail.com>
2024-02-21 23:15:09 +0300

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintArrayRecurs(int[] arr, int index)
+{
+    if (index >= arr.Length - 1)
+    {
+        Console.Write($"{arr[index]} ");
+        return;
+    }
+    PrintArrayRecurs(arr,index + 1);
+    Console.Write($"{arr[index]} ");
+}
+int[] array = {1,2,3,4,5,6};
+PrintArrayRecurs(array,0);
