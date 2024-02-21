5047da60099399d0c6f9d15c1103697e05a50b3d
golden lira <empirecheat17@gmail.com>
2024-02-21 22:57:43 +0300

    Add Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите стартовое число : ");
+int StartNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число : ");
+int EndNum = Convert.ToInt32(Console.ReadLine());
+PrintNatNumber(EndNum, StartNum);
+
+void PrintNatNumber(int end, int start)
+{
+    Console.Write($"{start} ");
+    if (start == end)
+    {
+        return;
+    }
+    PrintNatNumber(end, start + 1);
+}
