d74d272e0a30428604097f5c06a459815d1429c0
golden lira <empirecheat17@gmail.com>
2024-02-21 23:26:12 +0300

    Add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int AkkermanFunc(int n, int m)
+{
+    if (n == 0)
+    {
+        return m++;
+    }
+    else if (m == 0)
+    {
+        return AkkermanFunc(n - 1, 1);
+    }
+    return AkkermanFunc(n - 1, AkkermanFunc(n, m - 1));
+}
+
+Console.WriteLine(AkkermanFunc(3, 2));
